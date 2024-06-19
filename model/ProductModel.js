e56a3495776aea3a86108a4072ebb7c4487da959
wahyunup <wahyunurpambuko@gmail.com>
2024-06-19 20:53:59 +0700

    add database query

--- model/ProductModel.js
+++ model/ProductModel.js
@@ -4,9 +4,19 @@ import db from "../config/Database.js";
 const {DataTypes} = Sequelize;
 
 const products = db.define('product',{
-    name: DataTypes.string,
-    image: DataTypes.string,
-    url: DataTypes.string
+    name: {
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull:true,
+    },
+    url: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        defaultValue:0,
+    },
 },{
     freezeTableName: true
 });
--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -7,12 +7,12 @@ import {
     deleteProduct
 }from"../controller/ProductController.js";
 
+const router = express.Router();
+
 router.get('/products', getProducts);
 router.get('/products/:id', getProductById);
 router.post('/products', saveProduct);
 router.patch('/products/:id', updateProduct);
 router.delete('/products/:id', deleteProduct);
-
-const router = express.Router();
 
 export default router;
